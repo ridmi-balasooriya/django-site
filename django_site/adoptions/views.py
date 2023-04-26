3de8bb7b4a687bed8e559ff0ba72fe08726af34c
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-04-26 12:29:48 +0400

    Update views.py file for rendering templates and quering Pet object. Add template folder to addoption folder and and home and pet details template to it.

--- django_site/adoptions/views.py
+++ django_site/adoptions/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 
 def home(request):
-    return HttpResponse('<p>Home View</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets, })
 
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>pet_detail view with id {pet_id}</p>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found!')
+
+    return render(request, 'pet_detail.html', {'pet': pet})
